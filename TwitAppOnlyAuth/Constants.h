25b17e2a61f9e88720f251857291857b995d4edc
Michael Vilabrera <m.vilabrera@gmail.com>
2015-05-12 12:21:34 -0400

    twitter feed success

---
+++ TwitAppOnlyAuth/Constants.h
@@ -0,0 +1,16 @@
+//
+//  Constants.h
+//  TwitAppOnlyAuth
+//
+//  Created by Michael Vilabrera on 5/12/15.
+//  Copyright (c) 2015 Giving Tree. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+extern NSString *const TWITTER_CONSUMER_KEY;
+extern NSString *const TWITTER_CONSUMER_SECRET;
+
+@end
--- TwitAppOnlyAuth/ViewController.m
+++ TwitAppOnlyAuth/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+#import <STTwitter/STTwitter.h>
+#import "Constants.h"
 
-@interface ViewController ()
+NSString *const CellIdentifier = @"CellID";
+
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic) UITableView *tableView;
+@property (nonatomic) NSMutableArray *twitterFeed;
 
 @end
 
@@ -17,6 +24,56 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self createViews];
+    STTwitterAPI *twitter = [STTwitterAPI twitterAPIAppOnlyWithConsumerKey:TWITTER_CONSUMER_KEY consumerSecret:TWITTER_CONSUMER_SECRET];
+    [twitter verifyCredentialsWithSuccessBlock:^(NSString *username) {
+        //
+        [twitter getUserTimelineWithScreenName:@"mvilabrera" successBlock:^(NSArray *statuses) {
+            //
+            self.twitterFeed = [NSMutableArray arrayWithArray:statuses];
+            [self.tableView reloadData];
+        } errorBlock:^(NSError *error) {
+            //
+            NSLog(@"Error: %@", error.debugDescription);
+        }];
+    } errorBlock:^(NSError *error) {
+        //
+        NSLog(@"Error: %@", error.debugDescription);
+    }];
+}
+
+- (void)createViews
+{
+    CGRect tableViewFrame = CGRectMake(30, 30, 300, 500);
+    self.tableView = [[UITableView alloc] initWithFrame:tableViewFrame style:UITableViewStylePlain];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    [self.view addSubview:self.tableView];
+}
+
+#pragma mark -- TableView methods
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.twitterFeed.count;
+}
+
+- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (nil == cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    NSInteger idx = indexPath.row;
+    NSDictionary *t = self.twitterFeed[idx];
+    cell.textLabel.text = t[@"text"];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // when you tap the cell, it will deselect right away (just because)
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
