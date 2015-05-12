b22f5dcc2d33061984fe25ada34532f478ecccdd
Michael Vilabrera <m.vilabrera@gmail.com>
2015-05-12 11:01:47 -0400

    initial commit

---
+++ TwitAppOnlyAuth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitAppOnlyAuth
+//
+//  Created by Michael Vilabrera on 5/12/15.
+//  Copyright (c) 2015 Giving Tree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitAppOnlyAuth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitAppOnlyAuth
+//
+//  Created by Michael Vilabrera on 5/12/15.
+//  Copyright (c) 2015 Giving Tree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
